2079ea94b8b3eb19032ffc8fa8b6d352094689f2
Gabriel <gabrielpereirapinto33@gmail.com>
2024-01-24 12:32:19 -0300

    SmartTv

---
+++ Nova pasta/Sistema-Smart-tv/src/Usuario.java
@@ -0,0 +1,24 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal atual " + smartTv.canal);
+        System.out.println("Tv ligada " + smartTv.ligada);
+        System.out.println("Canal atual " + smartTv.canal);
+        System.out.println("volume atual " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> Tv ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> Tv ligada? " + smartTv.ligada);
+    }
+}
